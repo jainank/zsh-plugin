08df117fbba4cbb3e168e6a74238d067ba364da1
jainank <ankitjain5019@gmail.com>
2020-06-17 15:28:06 -0400

    init commit

---
+++ jainank.plugin.zsh
@@ -0,0 +1,9 @@
+export ANDROID_SDK_ROOT="~/Library/Android/sdk"
+export ANDROID_AVD_HOME="~/.android/avd"
+alias clr="clear"
+alias npr="npm run"
+alias ltr="ls -ltr"
+alias ltra="ls -ltra"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_212.jdk/Contents/Home"
+export PATH=$PATH:/Users/ankitjain/Library/Android/sdk/platform-tools
+alias glist="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
