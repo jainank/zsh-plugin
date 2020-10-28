3efa44cf252197baeaba620efcfe0453cebc305c
jainank <ankitjain5019@gmail.com>
2020-10-28 11:50:43 -0400

    Update jainank.plugin.zsh

--- jainank.plugin.zsh
+++ jainank.plugin.zsh
@@ -1,9 +1,15 @@
-export ANDROID_SDK_ROOT="~/Library/Android/sdk"
-export ANDROID_AVD_HOME="~/.android/avd"
+# set Android ENV variables
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_212.jdk/Contents/Home
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export PATH=$PATH:$ANDROID_HOME
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$PATH:$ANDROID_HOME/build-tools
+
+# userful aliase
 alias clr="clear"
 alias npr="npm run"
 alias ltr="ls -ltr"
 alias ltra="ls -ltra"
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_212.jdk/Contents/Home"
-export PATH=$PATH:/Users/ankitjain/Library/Android/sdk/platform-tools
 alias glist="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
